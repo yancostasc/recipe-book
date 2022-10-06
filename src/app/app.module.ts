b431e4e9565fad9989a31d1149d81be4cc6c9333
Yan <yan.costa@projuris.com.br>
2022-10-06 16:24:24 -0300

    Initial Commit 2 | Removing section 2 commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
